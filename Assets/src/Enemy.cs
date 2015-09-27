e2d621510fffb7ae442b68c191e82c492ee1d935
FlynnsPixel <flynnspixel@gmail.com>
2015-09-28 08:44:23 +1000

    Small enemy path follow fix

--- Assets/src/Enemy.cs
+++ Assets/src/Enemy.cs
@@ -4,17 +4,21 @@ using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour {
 
-    List<WaypointNode> path;
-    WaypointNode next_node;
+    List<WaypointNode> path = null;
+    WaypointNode next_node = null;
+    WaypointNode current_node = null;
     int current_path_index;
     Vector3 pos;
     float angle;
     Vector2 accel;
 
+    float friction = .99f;
+    float max_accel = .05f;
+
 	void Start() {
         pos = WaypointGrid.instance.grid_to_world(WaypointGrid.instance.world_to_grid(transform.position));
+        current_node = WaypointGrid.instance.get_node(WaypointGrid.instance.world_to_grid(pos));
 
-        Vector2 dest = GameObject.Find("goal").transform.position;
         update_path();
         foreach (WaypointNode n in path)
         {
@@ -27,9 +31,11 @@ public class Enemy : MonoBehaviour {
     
     bool update_path()
     {
-        Vector2 dest = GameObject.Find("goal").transform.position;
-        List<WaypointNode> temp_path = WaypointGrid.instance.find_path(transform.position, dest);
-        if (temp_path.Count != 0)
+        Vector2 dest = WaypointGrid.instance.world_to_grid(GameObject.Find("goal").transform.position);
+        List<WaypointNode> temp_path = WaypointGrid.instance.find_path((int)current_node.grid_pos.x, (int)current_node.grid_pos.y,
+                                                                       (int)dest.x, (int)dest.y);
+
+        if (temp_path.Count >= 2)
         {
             path = temp_path;
             return true;
@@ -49,14 +55,16 @@ public class Enemy : MonoBehaviour {
 
     void Update() {
         update_angle();
-        accel.x += Mathf.Cos(angle) / 200.0f;
-        accel.y += Mathf.Sin(angle) / 200.0f;
-        accel.x *= .99f;
-        accel.y *= .99f;
-        accel.x = Mathf.Clamp(accel.x, -.05f, .05f);
-        accel.y = Mathf.Clamp(accel.y, -.05f, .05f);
+        accel.x += Mathf.Cos(angle) * (smoothness * max_accel);
+        accel.y += Mathf.Sin(angle) * (smoothness * max_accel);
+        accel.x *= friction;
+        accel.y *= friction;
+        accel.x = Mathf.Clamp(accel.x, -max_accel, max_accel);
+        accel.y = Mathf.Clamp(accel.y, -max_accel, max_accel);
         pos.x += accel.x;
         pos.y += accel.y;
+
+        current_node = WaypointGrid.instance.get_node(WaypointGrid.instance.world_to_grid(pos));
 
         float dist = Mathf.Sqrt(Mathf.Pow(pos.x - next_node.world_pos.x, 2) + Mathf.Pow(pos.y - next_node.world_pos.y, 2));
         if (dist < .5f)
@@ -64,10 +72,9 @@ public class Enemy : MonoBehaviour {
             //++current_path_index;
             //get_next_path_node();
 
-            Vector2 dest = GameObject.Find("goal").transform.position;
             if (update_path())
             {
-                current_path_index = 0;
+                current_path_index = 1;
                 get_next_path_node();
             }
         }
